9b01dac92fde7c70d092bde8b40aa4d5541f3fbb
PaulNewman-M <[paulnewman.m@gmail.com]>
2017-10-30 00:44:18 -0700

    completed color game

--- colorGame.js
+++ colorGame.js
@@ -1,36 +1,46 @@
-var color=[
-          "rgb(255, 0, 0)",
-          "rgb(255, 255, 0)",
-          "rgb(255, 255,255)",
-          "rgb(0, 255, 255)",
-          "rgb(0, 255, 0)",
-          "rgb(0, 0, 244)"
-
-          ]
-
- var colorpicked=color[3];         
-
+var color=randomColorGenerator(6);
+var colorpicked=color[Math.floor(Math.random()*6+1)];         
 var squares=document.querySelectorAll(".square");
-console.log("square number is ",squares,"length",squares.length);
+var text=document.querySelector(".text");
+var reset=document.querySelector(".reset");
 
+reset.addEventListener("click", function(){
+	location.reload();
+})
+
+
+console.log("square number is ",squares,"length",squares.length);
 
 var rgb=document.querySelector(".colordisplayed");
 rgb.textContent=colorpicked;
 for(var i=0;i<squares.length;i++){
 
-    squares[i].style.backgroundColor=color[i];
-
-     squares[i].addEventListener( "click", function(){
+    squares[i].style.background=color[i];
+console.log(color[i],"bg color",squares[i].style.backgroundColor);
+    squares[i].addEventListener( "click", function(){
 
       var clickedcolor = this.style.backgroundColor;
       console.log("clickedcolor:",clickedcolor);
-     if (clickedcolor==colorpicked){
-	     alert("correct");}
+     if (clickedcolor===colorpicked){
+	     text.textContent="Correct!";}
 	 else{
-		alert("wrong");
+	 	text.textContent="try again!";
      	}
+}); 
 
-});
+}
+
+function randomColorGenerator(numberOfColors){
+var arr=[];
+for(var i=0;i<numberOfColors;i++){
+var 	r=Math.floor(Math.random()*256);
+var 	g=Math.floor(Math.random()*256);
+var 	b=Math.floor(Math.random()*256);
+arr.push("rgb("+ r +", "+ g +", "+ b +")");
+console.log(arr);
+}
+return arr;
+
 }
 
 
